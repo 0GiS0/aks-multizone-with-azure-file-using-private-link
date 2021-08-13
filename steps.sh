6701000d39c7af2b762477af7f14a338bfaf3db3
0GiS0 <giselatb@outlook.com>
2021-08-13 10:39:45 +0200

    Create storage account and private link dynamically

--- steps.sh
+++ steps.sh
@@ -1,106 +1,40 @@
 #Variables
-RESOURCE_GROUP="AKS-Demo"
-AKS_NAME="ha-aks"
+RESOURCE_GROUP="AKS-DynamicStorage"
+AKS_NAME="aks-dynamicstore"
 LOCATION="northeurope"
-VNET="aks-vnet"
-SUBNET_AKS="aks-subnet"
-SUBNET_ENDPOINTS="endpoints"
-PRIVATE_DNS_ZONE="private.file.core.windows.net"
-PRIVATE_STORAGE_ACCOUNT="webapache"
-RECORD_SET_NAME="web-apache-storage"
 
 #Create Resource Group
 az group create --name $RESOURCE_GROUP --location $LOCATION
-
-#Create a VNET
-az network vnet create --resource-group $RESOURCE_GROUP \
---name $VNET --address-prefixes 192.168.0.0/16 \
---subnet-name $SUBNET_AKS \
---subnet-prefixes 192.168.1.0/24
-
-VNET_ID=$(az network vnet show --name $VNET --resource-group $RESOURCE_GROUP --query "id" --output tsv)
-SUBNET_AKS_ID=$(az network vnet subnet show --name $SUBNET_AKS --resource-group $RESOURCE_GROUP --vnet-name $VNET --query "id" --output tsv)
-
-#Create a subnet for the endpoints
-az network vnet subnet create --resource-group $RESOURCE_GROUP \
---vnet-name $VNET --name $SUBNET_ENDPOINTS --address-prefixes 192.168.2.0/24 \
---disable-private-endpoint-network-policies true
-
-#Create a private DNS zone
-az network private-dns zone create --resource-group $RESOURCE_GROUP \
---name $PRIVATE_DNS_ZONE
-
-#Create an association between the VNET and the private DNS zone
-az network private-dns link vnet create \
---name "link_between_dns_and_vnet" \
---resource-group $RESOURCE_GROUP \
---zone-name $PRIVATE_DNS_ZONE \
---virtual-network $VNET \
---registration-enabled false
-
-# Create an Azure Storage only accesible via private link
-# So you need:
-# 1. Create a storage account
-az storage account create -g $RESOURCE_GROUP \
---name $PRIVATE_STORAGE_ACCOUNT \
---sku Standard_ZRS \
---default-action Deny
-
-STORAGE_ACCOUNT_ID=$(az storage account show --name $PRIVATE_STORAGE_ACCOUNT --resource-group $RESOURCE_GROUP --query "id" --output tsv)
-
-# 2. Create a private endpoint for the storage account
-az network private-endpoint create --resource-group $RESOURCE_GROUP \
---name $PRIVATE_STORAGE_ACCOUNT \
---vnet-name $VNET \
---subnet $SUBNET_ENDPOINTS \
---location $LOCATION \
---connection-name $PRIVATE_STORAGE_ACCOUNT \
---group-id file \
---private-connection-resource-id $STORAGE_ACCOUNT_ID 
-
-# 3. Get the ID of the azure storage NIC
-STORAGE_NIC_ID=$(az network private-endpoint show --name $PRIVATE_STORAGE_ACCOUNT -g $RESOURCE_GROUP --query 'networkInterfaces[0].id' -o tsv)
-
-# 4. Get the IP of the azure storage NIC
-STORAGE_ACCOUNT_PRIVATE_IP=$(az resource show --ids $STORAGE_NIC_ID --query 'properties.ipConfigurations[0].properties.privateIPAddress' --output tsv)
-
-# 5. Setup DNS with the new private endpoint
-# The record set name to the private IP of the storage account
-az network private-dns record-set a add-record \
---record-set-name $RECORD_SET_NAME \
---resource-group $RESOURCE_GROUP \
---zone-name $PRIVATE_DNS_ZONE \
---ipv4-address $STORAGE_ACCOUNT_PRIVATE_IP
-
-
-#Create a Service Principal
-az ad sp create-for-rbac -n $AKS_NAME --role Contributor \
---scopes $STORAGE_ACCOUNT_ID $VNET_ID > auth.json
-
-appId=$(jq -r ".appId" auth.json)
-password=$(jq -r ".password" auth.json)
 
 #Create AKS cluster
 az aks create \
     --resource-group $RESOURCE_GROUP \
     --name $AKS_NAME \
     --generate-ssh-keys \
-    --load-balancer-sku standard \
     --node-count 3 \
-    --zones 1 2 3 \
-    --vnet-subnet-id $SUBNET_AKS_ID \
-    --service-principal $appId \
-    --client-secret $password
-    
+    --zones 1 2 3
 
+#Get AKS node resource group name
+AKS_RESOURCE_GROUP=$(az aks show --resource-group $RESOURCE_GROUP --name $AKS_NAME --query nodeResourceGroup --output tsv)
+
+#Get AKS node resource group id
+AKS_RESOURCE_GROUP_ID=$(az group show --name $AKS_RESOURCE_GROUP --query id --output tsv)
+
+#Get Managed identity id
+MANAGED_IDENTITY_ID=$(az aks show --name ${AKS_NAME} -g $RESOURCE_GROUP --query identityProfile.kubeletidentity.clientId --output tsv)
+
+# Give Contributor permissions to the managed identity of the cluster
+az role assignment create --assignee $MANAGED_IDENTITY_ID --role contributor --scope $AKS_RESOURCE_GROUP_ID
+    
 # Get AKS credentials
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_NAME
 
 #See the zone for the nodes
 kubectl get nodes -o custom-columns=NAME:'{.metadata.name}',REGION:'{.metadata.labels.topology\.kubernetes\.io/region}',ZONE:'{metadata.labels.topology\.kubernetes\.io/zone}'
 
-# Install CSI Driver for Azure Files
+# Install CSI Driver
 curl -skSL https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/v1.5.0/deploy/install-driver.sh | bash -s v1.5.0 --
+k get pod -A
 
 # Create Apache Web server using Azure Files
 kubectl apply -f k8s/.
